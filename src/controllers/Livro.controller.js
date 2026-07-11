bf2d90e5a1f058f83deba524bf9ccde1e7a8b480
Eliel Bombieri <bombieri404@gmail.com>
2026-07-11 13:17:30 -0300

    feat: populate author on book update; format fixes

--- src/controllers/Livro.controller.js
+++ src/controllers/Livro.controller.js
@@ -8,7 +8,7 @@ class livroController {
             const livros = await livro.find({});
             res.status(200).json(livros);
 
-        } catch(erro) {
+        } catch (erro) {
             res.status(500).json({ message: `Falha na listagem dos livros: ${erro.message}` })
         }
     }
@@ -18,46 +18,54 @@ class livroController {
             const id = req.params.id;
             const livroEncontrado = await livro.findById(id);
             res.status(200).json(livroEncontrado);
-        } catch(erro) {
+        } catch (erro) {
             res.status(500).json({ message: `Falha na listagem dos livros: ${erro.message}` })
         }
     }
-    
+
     static async criarLivro(req, res) {
         const novoLivro = req.body
 
         try {
             const autorEncontrado = await autor.findById(novoLivro.autor);
             const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado } }
-            
+
             const livroCriado = await livro.create(livroCompleto);
             res.status(200).json({
                 message: "Criado com sucesso!",
                 livro: livroCriado
             });
-        } catch(erro) {
+        } catch (erro) {
             res.status(500).json({
                 message: `Erro ao cadastrar livro: ${erro.message}`
             })
         }
     }
-    
+
     static async alterarLivro(req, res) {
+        const novoLivro = req.body
+        const id = req.params.id;
+
         try {
-            const id = req.params.id;
-            await livro.findByIdAndUpdate(id, req.body);
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            if (autorEncontrado) {
+                const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado } }
+                await livro.findByIdAndUpdate(id, livroCompleto);
+            } else {
+                await livro.findByIdAndUpdate(id, req.body);
+            }
             res.status(200).json({ message: "Livro alterado com sucesso!" });
-        } catch(erro) {
+        } catch (erro) {
             res.status(500).json({ message: `Falha na alteração do livro: ${erro.message}` })
         }
     }
-    
+
     static async removerLivro(req, res) {
         try {
             const id = req.params.id;
             await livro.findByIdAndDelete(id);
             res.status(200).json({ message: "Livro excluido com sucesso!" });
-        } catch(erro) {
+        } catch (erro) {
             res.status(500).json({ message: `Falha na exclusão do livro: ${erro.message}` })
         }
     }
